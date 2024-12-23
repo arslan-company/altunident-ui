63fe90c853e93c83aa5ab9ec7bdbb3d93209d3a9
Okan <okan.unallar@gmail.com>
2024-12-23 16:04:05 +0300

    tailwind css setup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,8 @@
-'use client'
-
 import React from 'react'
 import { useTranslations } from 'next-intl'
 
 export default function Page() {
   const t = useTranslations('homepage.services')
 
-  return (
-    <div> {t('description')} </div>
-  )
+  return <div>{t('description')}</div>
 }
