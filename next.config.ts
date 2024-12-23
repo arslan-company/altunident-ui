a94bdc4818aa6ce08a866ef49febe2d369b2e997
Okan <okan.unallar@gmail.com>
2024-12-23 16:03:53 +0300

    routing structure updated

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,20 @@
-import path from 'node:path';
+import path from 'node:path'
 
-import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   env: {
     API_URL: process.env.API_URL,
   },
-  output: 'standalone',
   compiler: {
     emotion: true,
   },
-};
+}
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(nextConfig)
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,20 @@
-import createMiddleware from 'next-intl/middleware';
+import createMiddleware from 'next-intl/middleware'
 
-import {routing} from '@/i18n/routing';
+import { routing } from '@/i18n/routing'
 
-export default createMiddleware(routing);
-
-const locales = routing.locales;
+export default createMiddleware(routing)
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', `/(${locales.join('|')})/:path*`]
-};
+  matcher: [
+    // Enable a redirect to a matching locale at the root
+    '/',
+
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    `/(tr|en)/:path*`,
+
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    '/((?!_next|_vercel|.*\\..*).*)',
+  ],
+}
