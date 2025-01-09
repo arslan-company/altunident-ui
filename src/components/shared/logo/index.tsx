62d689bc4f6c2b7ecd212c070da5f5829f42c7e5
Okan <okan.unallar@gmail.com>
2025-01-09 16:11:51 +0300

    logo component added

---
+++ src/components/shared/logo/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+import { useTranslations } from 'next-intl';
+
+import { Media } from '@/features/files';
+import { cn } from '@/lib/cn';
+
+const logoVariants = {
+  primary: '/img/logos/logo-primary.svg',
+  white: '/img/logos/logo-white.svg',
+};
+
+interface LogoProps {
+  className?: string;
+  variant?: keyof typeof logoVariants;
+}
+
+export default function Logo({ className, variant = 'primary' }: LogoProps) {
+  const t = useTranslations();
+
+  return (
+    <Media
+      src={logoVariants[variant]}
+      element="image"
+      imageProps={{ alt: `${t('site.name')} logo`, width: 120, height: 24 }}
+      className={cn('tw-w-auto tw-h-14', className)}
+    />
+  );
+}
