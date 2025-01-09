7d77fc716311bcc7e7ae9f5bfef53e96ae518a00
Okan <okan.unallar@gmail.com>
2025-01-09 16:10:03 +0300

    darker colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ module.exports = {
           light: 'hsl(var(--primary-light))',
         },
         darker: {
-          DEFAULT: 'hsl(var(--darker))',
+          100: 'hsl(var(--darker-100))',
+          200: 'hsl(var(--darker-200))',
+          300: 'hsl(var(--darker-300))',
+          400: 'hsl(var(--darker-400))',
+          500: 'hsl(var(--darker-500))',
+          600: 'hsl(var(--darker-600))',
+          700: 'hsl(var(--darker-700))',
+          800: 'hsl(var(--darker-800))',
+          900: 'hsl(var(--darker-900))',
         },
       },
       keyframes: {
@@ -37,10 +45,32 @@ module.exports = {
             transform: 'translateY(20px)',
           },
         },
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+        loadingBar: {
+          '0%, 100%': {
+            transform: 'scaleY(0.5)',
+            opacity: '0.5',
+          },
+          '50%': {
+            transform: 'scaleY(1)',
+            opacity: '1',
+          },
+        },
       },
       animation: {
         contentShow: 'contentShow 300ms cubic-bezier(0.16, 1, 0.3, 1)',
         contentHide: 'contentHide 300ms cubic-bezier(0.16, 1, 0.3, 1)',
+        bounce: 'bounce 1s infinite',
+        loadingBar: 'loadingBar 1s ease-in-out infinite',
       },
       fontFamily: {
         sans: ['var(--font-inter)'],
