67e6dd35fddf3a7210c3dab7d0e15963548b3adf
Okan <okan.unallar@gmail.com>
2025-01-10 16:27:37 +0300

    kvkk & cookie pages updated

--- src/app/[locale]/kvkk-aydinlatma-metni/page.tsx
+++ src/app/[locale]/kvkk-aydinlatma-metni/page.tsx
@@ -23,159 +23,103 @@ export default async function KVKKPage() {
       <main className="tw-min-h-screen tw-py-12">
         <div className="container tw-mx-auto tw-px-4 tw-space-y-6">
           <h3>KVKK (Kişisel Verilerin Korunması Konunu)</h3>
-          <br />
-          <br />
-          <h6>KİŞİSEL VERİLERİN KORUNMASI AYDINLATMA METNİ VE AÇIK RIZA FORMU</h6>
-          <br />
-          6698 Sayılı Kişisel Verilerin Korunması Kanunu Kapsamında, Kişisel Verilerinizi sınırlı
-          bağlantılı ve ölçülü olarak işlemekteyiz. Cihan Özel Sağlık ve Eğitim Hiz. İnş. San. Tic.
-          A. Ş. (“Hastane”) olarak Veri Sorumlusu sıfatıyla, Kişisel Verilerinizi talep ettiğimiz ve
-          bizimle paylaşmış olduğunuz kişisel verilerinizin işlenmelerini gerektiren amaç
-          çerçevesinde ve bu amaç ile bağlantılı, sınırlı ve ölçülü şekilde, doğruluğunu ve en
-          güncel halini koruyarak aşağıda açıklanan kapsamda işliyoruz. Bu konuda sizleri detaylı
-          olarak bilgilendirmek ve veri işlemenin açık rızanıza tabi olduğu konularda sizlerden onay
-          almak istiyoruz.
-          <br />
-          <br />
-          <h6>Hangi Kişisel Verilerinizi İşliyoruz;</h6>
-          İşlemekte olduğumuz kişisel verileriniz; Ad-soyad, TCKN, ve kimilk belgeleriniz üzerinden
-          yer alan tüm bilgileriniz dahil kimlik verileriniz; İkamet adresiniz, telefon numaranız,
-          mail adresiniz dahil iletişim bilgileriniz; yine acil durumlarda, refakat süreçlerinde
-          iletişim kurulacak kişiler gibi aile veya yakın bilgileriniz; banka hesap numarası, IBAN
-          numarası bilgileri dahil finansal bilgileriniz, sizin tarafınızdan sunulan veya
-          hastanemizce yapılan tüm tıbbi teşhis, tedavi ve bakım hizmetleri kapsamında hastalık
-          bilgilerinizi içeren tüm belgeler/bilgiler dahil sağlık/cinsel verileriniz; Sağlık
-          hizmetlerinin finansmanı ve planlaması amacıyla özel sağlık sigortasına ilişkin
-          verileriniz; Hastanemiz ziyaretiniz süresince kapalı devre görüntü sistemleri vasıtasıyla
-          sizlerden aldığımız görüntü ve araç plaka verileriniz dahil olmak üzere görüntü
-          verileriniz; parmak izi, avuç içi, damar izi gibi mevzuattan kaynaklı işlenmesi gereken
-          biometrik verileriniz; yine pazarlama ve iletişim faaliyetlerinde kullanılmak üzere
-          hastanemiz içerisinde alınmış görüntü verileriniz; çağrı merkezi üzerinden vermiş
-          olduğunuz işitsel verileriniz ve bunlarla sınırlı olmayan kişisel bilgilerden
-          oluşabilecektir.
-          <br />
-          <br />
-          <h6>Kişisel Verileriniz İşleme Amaçlarımız</h6>
-          Toplanan kişisel verileriniz, Hastanemiz ve Hastanemizle iş ilişkisi içerisinde olan
-          kişilerin hukuki ve ticari güvenliğinin temini, Hastanemiz tarafından hastane sahasının
-          fiziksel mekân güvenliğinin temini, ziyaretçi/hasta kayıtlarının oluşturulması ve takibi,
-          ilgili/yetkili kamu kuruluşlarına bildirim yükümlülüğünün yerine getirilmesi (mahkemeler,
-          kolluk kuvvetleri gibi), 6331 sayılı kanun gereği İş Sağlığı ve Güvenliği çalışmalarının
-          yerine getirilmesi, satış pazarlama faaliyetlerinin yürütülmesi, kimliğinizi teyit etme,
-          kamu sağlığının korunması, koruyucu hekimlik, tıbbî teşhis, tedavi ve bakım hizmetlerinin
-          yürütülmesi, sağlık hizmetleri ile finansmanının planlanması ve yönetimi, Hastanemizin iç
-          işleyişi ile günlük operasyonların planlanması ve yönetilmesi, ilaç temini, randevu
-          almanız halinde randevu hakkında sizi bilgilendirebilme, risk yönetimi ve kalite
-          geliştirme faaliyetlerinin yerine getirilmesi, sağlık hizmetlerinin geliştirmesi amacıyla
-          değerlendirmelerde bulunma, araştırma yapılması, Hastanemiz ile anlaşmalı olan kurumlarla
-          ilişkinizin teyit edilmesi, sağlık hizmetlerimiz karşılığında faturalandırma yapılması,
-          sağlık hizmetlerinin finansmanı kapsamında özel sigorta şirketleri ile talep edilen
-          bilgilerin paylaşılması, ilgili mevzuat uyarınca Sağlık Bakanlığı ve ilgili kamu kurum ve
-          kuruluşları ile talep edilen bilgilerin paylaşılması, sağlık hizmetlerimize ilişkin her
-          türlü soru ve şikâyetinize cevap verilmesi, Hastanemizin sistem ve uygulamalarının veri
-          güvenliği kapsamında tüm gerekli teknik ve idari tedbirlerin alması, sunduğumuz sağlık
-          hizmetlerinin geliştirilmesi ve iyileştirilmesi amacıyla sağlık hizmetleri kullanımınızın
-          analiz edilmesi ve sağlık verilerinizi saklanması, düzenleyici ve denetleyici kurumlarla,
-          resmi mercilerin talep ve denetimleri doğrultusunda gerekli bilgilerin temin edilmesi,
-          çalışanlarımızın eğitimi ve geliştirilmesi, suiistimal ve yetkisiz işlemlerin izlenmesi,
-          engellenmesi ve işlemlerin geri alınması, ilgili mevzuat gereği saklanması gereken sağlık
-          verilerinize ilişkin bilgilerin muhafaza edilmesi, anlaşmalı olduğumuz kurumlarla size
-          sunulan sağlık hizmetlerine ilişkin finansal mutabakat sağlanması, hasta memnuniyetinin
-          ölçülmesi ve bunlarla sınırlı olmaksızın, tıbbî teşhis, tedavi ve bakım hizmetlerinin
-          yürütülmesi, geliştirilmesi, hasta memnuniyetinin arttırılması, araştırma ve benzeri
-          amaçlar, hastanemiz hizmetlerimiz, yenilik ve kampanyalarımızla ilgili pazarlama, tanıtım
-          ve iletişim faaliyetlerinde kullanılmak gibi amaçlarla işlenebilecektir. Bu amaçlarla
-          kişisel verileriniz kaydedilebilir.
-          <br />
-          <br />
-          <h6>Kişisel Verilerinizin Aktarılması</h6>
-          Yukarıda sayılan amaçlarla verileriniz işlenir, güvenle saklanır, kamu kuruluşlarına
-          bildirim yükümlülüğü, yasal mevzuat ve açık rızanız dışında 3. kişilerle kesinlikle
-          paylaşılmaz. Kişisel verileriniz; Sosyal Güvenlik Kurumu, Özel sigorta şirketleri (sağlık,
-          emeklilik, hayat sigortası ve benzeri), İşverenleriniz aracı sağlık kuruluşları, yetkili
-          kamu kurum/kuruluşlarına, ürün veya hizmet aldığımız aracı firmalara, kişilere, iş
-          süreçlerimizde destek aldığımız yerel/uluslararası kuruluşlara; 5510 Sosyal Sigortalar ve
-          Genel Sağlık Sigortası Kanunu 3359 sayılı Sağlık Hizmetleri Temel Kanunu, 663 sayılı
-          Sağlık Bakanlığı ve Bağlı Kuruluşlarının Teşkilat ve Görevleri Hakkında Kanun Hükmünde
-          Kararname, Sağlıkta Kalite Standartları(SKS), Özel Hastaneler Yönetmeliği, VUK, TTK, İSG
-          Kanunu gibi yasal mevzuattan kaynaklı yükümlülükler gereği hukuki dayanaklar ve rızanız
-          çerçevesinde aktarılabilecektir.
-          <br />
-          <br />
-          <h6>Kişisel Verilerinizin Toplanma Yöntemi</h6>
-          Kişisel verileriniz Hastanemiz tarafından farklı kanallarla ve farklı hukuki sebeplere
-          dayanarak toplanmaktadır. Bu çerçevede; refakatçi taahhütnamesi, özel hasta taahhütnamesi,
-          SGK yatan hasta taahhütnamesi vb. taahhütnameler, Hastan onam ve rıza formları, kapalı
-          devre görüntü sistemleri, kayıt formları, talep/şikayet/iletişim formları, çağrı merkezi
-          ve web sitemiz gibi kanallar aracılığıyla sözlü, yazılı veya elektronik ortamda,
-          yurtiçi/yurtdışı iş birliği yaptığımız program ortağı kurum ve kuruluşlar, resmî kurumlar,
-          diğer 3. kişiler de dâhil olmak üzere Hastanemiz dışından da elde edilebilecek şekilde
-          fiziksel ortamlarda kâğıt üzerinde veya dijital ortamlarda elektronik yöntemlerle
-          toplanmaktadır.
-          <br />
-          <br />
-          <h6>KVK Kanunu’nun 11. maddesinde Sayılan Haklarınız</h6>
-          Kişisel veri sahipleri olarak, haklarınıza ilişkin taleplerinizi, işbu Aydınlatma Metninde
-          aşağıda düzenlenen yöntemlerle Hastanemize iletmeniz durumunda Hastanemiz talebin
-          niteliğine göre talebi en kısa sürede ve en geç 30 gün içinde ücretsiz olarak
-          sonuçlandıracaktır. Ancak, Kişisel Verileri Koruma Kurulunca bir ücret öngörülmesi
-          halinde, Hastanemiz tarafından belirlenen tarifedeki ücret alınacaktır. Bu kapsamda
-          kişisel veriniz ile ilgili a - Kişisel veri işlenip işlenmediğini öğrenme, b - Kişisel
-          verileri işlenmişse buna ilişkin bilgi talep etme, c - Kişisel verilerin işlenme amacını
-          ve bunların amacına uygun kullanılıp kullanılmadığını öğrenme, ç - Yurt içinde veya yurt
-          dışında kişisel verilerin aktarıldığı üçüncü kişileri bilme, d - Kişisel verilerin eksik
-          veya yanlış işlenmiş olması hâlinde bunların düzeltilmesini isteme ve bu kapsamda yapılan
-          işlemin kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini isteme, e - KVK
-          Kanunu’nun ve ilgili diğer kanun hükümlerine uygun olarak işlenmiş olmasına rağmen,
-          işlenmesini gerektiren sebeplerin ortadan kalkması hâlinde kişisel verilerin silinmesini
-          veya yok edilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin aktarıldığı
-          üçüncü kişilere bildirilmesini isteme, f - İşlenen verilerin münhasıran otomatik sistemler
-          vasıtasıyla analiz edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya
-          çıkmasına itiraz etme, g - Kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle
-          zarara uğraması hâlinde zararın giderilmesini talep etme haklarına sahipsiniz.
-          <br />
-          <br />
-          <h6>Veri Sorumlusu ve İlgili Kişi Bilgi Talep Yöntemleri</h6>
-          KVK Kanunu gereği yukarıda belirtilen haklarınızı kullanmak ile ilgili talebinizi, yazılı
-          veya Kişisel Verileri Koruma Kurulu’nun belirlediği diğer yöntemlerle ve aşağıda
-          belirtilen kanallar ile Hastanemize iletebilirsiniz. İlgili kişi olarak hak taleplerinizi,
-          yazılı olarak kimliğinizi tespit edici gerekli belgeleri de sağlayarak Hastanemize
-          iletmeniz gerekmektedir. Bu çerçevede, Hastanemize KVK Kanunu’nun 11. maddesi kapsamında
-          yapacağınız başvuruların sağlıklı ve hızlı şekilde yönetilmesi için,
-          www.cihanhastanesi.com.tr adresinde Kişisel Verilerin Korunması başlığı altında “İlgili
-          Kişi Bilgi Talep Formu” belgesini kullanmanızı, talebinize göre istenebilecek
-          belge/bilgileri ve kimliğinizi tespit edici gerekli belgeleri de sağlayarak bizzat elden
-          ya da iadeli taahhütlü posta yolu ile yapılmasını öneriyoruz.
-          <br />
-          <br />
-          Ayrıca, ilgili kişi bilgi talep formunu kullanarak cihansaglik@hc01.kep.tr adresine
-          güvenli elektronik imzalı olarak ya da avukatınız veya noter aracılığı ile talebinizi
-          iletebilirsiniz.
-          <br />
-          <br />
-          <h6>Kişisel Verilerin Korunması ve İşlenmesi Hakkında Açık Rıza Metni</h6>
-          Kişisel verilerinizin işlenmesine onayınızı, lütfen aşağıda (x) şeklinde işaretleyerek
-          açıkça belirtiniz. Dilediğiniz zaman herhangi bir sınırlamaya tabi olmaksızın açık
-          rızanızı geri alabilirsiniz.
-          <br />
-          <br />
-          6698 Sayılı Kişisel Verilerin Korunması Kanunu Kapsamında kişisel verilerimin aydınlatma
-          metninde yer alan çerçeve ve amaçlar kapsamında muhafaza edilmesi, işlenmesi ve
-          aktarılmasını, kabul ve beyan ederim.
-          <br />
-          <br />
-          Kampanyalarınız, yenilikleriniz ve hizmetleriniz ile ilgili e-Posta, SMS ve telefonla
-          bilgi vermek için bana ulaşabileceğinizi kabul ve beyan ederim.
-          <br />
-          <br />
-          Aydınlatma metnini okudum, görüntü verilerimin Hastane tanıtım, pazarlama ve iletişim
-          faaliyetlerinde, web sitesi ve sosyal medya üzerinden paylaşılmasını kabul ve beyan
-          ederim. Cihan Özel Sağlık ve Eğitim Hiz. İnş. San. Tic. A. Ş. (“Hastane”) tarafından 6698
-          sayılı Kişisel Verilerin Korunması Kanunu (“KVK Kanunu”) kapsamında hazırlanmış olan
-          “Kişisel Verilerin Korunması Bilgilendirme ve Aydınlatma Metni”’nin (“Aydınlatma Metni”)
-          tamamını okudum, anladım ve kişisel verilerimin işlenme şartları ve açık rızamı dilediğim
-          zaman herhangi bir sınırlamaya tabi olmaksızın geri alabileceğim hakkında bilgi sahibi
-          oldum.
+          <div className="tw-space-y-8">
+            <section>
+              <h4 className="tw-text-xl tw-font-semibold tw-mb-4">1. Veri Sorumlusu</h4>
+              <div className="tw-space-y-2">
+                <p>Altunident Diş Kliniği</p>
+                <p>Adres: Altunizade, Mahir İz Cd. No:43, Üsküdar/İstanbul</p>
+                <p>Telefon: 444 8 000</p>
+                <p>E-posta: info@altunident.com</p>
+              </div>
+            </section>
+
+            <section>
+              <h4 className="tw-text-xl tw-font-semibold tw-mb-4">
+                2. Kişisel Verilerin İşlenme Amaçları
+              </h4>
+              <p>
+                Altunident Diş Kliniği olarak, müşteri memnuniyetini sağlamak ve hizmet kalitemizi
+                artırmak amacıyla aşağıdaki amaçlarla kişisel verilerinizi işlemekteyiz:
+              </p>
+              <ul className="tw-list-disc tw-pl-6 tw-mt-2 tw-space-y-1">
+                <li>Hizmetlerimizi sunmak ve geliştirmek</li>
+                <li>Randevu taleplerini işlemek</li>
+                <li>İletişim taleplerini yanıtlamak</li>
+                <li>Sağlık hizmetleri sunumu için gerekli olan bilgileri sağlamak</li>
+                <li>Sağlık turizmi hizmetlerimizi sunmak ve organize etmek</li>
+              </ul>
+            </section>
+
+            <section>
+              <h4 className="tw-text-xl tw-font-semibold tw-mb-4">3. İşlenen Kişisel Veriler</h4>
+              <p>
+                Altunident Diş Kliniği olarak, aşağıdaki kişisel veri kategorilerini işlemekteyiz:
+              </p>
+              <ul className="tw-list-disc tw-pl-6 tw-mt-2 tw-space-y-1">
+                <li>İletişim bilgileri (ad, soyad, e-posta adresi, telefon numarası vb.)</li>
+                <li>Sağlık bilgileri</li>
+                <li>Randevu talepleri ile ilgili bilgiler</li>
+                <li>İnternet sitesi kullanımıyla ilgili veriler (çerezler, IP adresleri vb.)</li>
+              </ul>
+            </section>
+
+            <section>
+              <h4 className="tw-text-xl tw-font-semibold tw-mb-4">
+                4. Kişisel Verilerin Saklama Süresi
+              </h4>
+              <p>
+                Kişisel veriler, yasal saklama sürelerine ve işlenme amaçlarına uygun olarak
+                saklanmaktadır. Veri sahipleri talep etmedikçe kişisel verilerinizi saklamayacağız.
+              </p>
+            </section>
+
+            <section>
+              <h4 className="tw-text-xl tw-font-semibold tw-mb-4">
+                5. Kişisel Veri Sahibi Hakları
+              </h4>
+              <p>KVKK kapsamında, veri sahiplerine aşağıdaki hakları tanımaktayız:</p>
+              <ul className="tw-list-disc tw-pl-6 tw-mt-2 tw-space-y-1">
+                <li>Kişisel veri işlenip işlenmediğini öğrenme</li>
+                <li>Kişisel verileri işlenmişse buna ilişkin bilgi talep etme</li>
+                <li>
+                  Kişisel verilerin işlenme amacını ve bunların amacına uygun kullanılıp
+                  kullanılmadığını öğrenme
+                </li>
+                <li>
+                  Kişisel verilerin yurt içinde veya yurt dışında aktarıldığı üçüncü kişileri bilme
+                </li>
+                <li>
+                  Kişisel verilerin eksik veya yanlış işlenmiş olması halinde bunların
+                  düzeltilmesini isteme
+                </li>
+                <li>
+                  KVKK ve ilgili diğer mevzuat hükümlerine uygun olarak kişisel verilerin
+                  silinmesini veya yok edilmesini talep etme
+                </li>
+                <li>
+                  Kişisel verilerin aktarıldığı üçüncü kişilere bu taleplerin bildirilmesini isteme
+                </li>
+                <li>
+                  İşlenen verilerin münhasıran otomatik sistemler vasıtasıyla analiz edilmesi
+                  suretiyle kişinin kendisi aleyhine bir sonucun ortaya çıkmasına itiraz etme
+                </li>
+                <li>
+                  Kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle zarara uğraması halinde
+                  zararın giderilmesini talep etme
+                </li>
+              </ul>
+            </section>
+
+            <section>
+              <h4 className="tw-text-xl tw-font-semibold tw-mb-4">6. Başvuru Yöntemi</h4>
+              <p>
+                KVKK kapsamında veri sahipleri yukarıdaki haklarını kullanmak için Altunident Diş
+                Kliniği ile iletişime geçebilirler. Başvurularınızı, yazılı olarak veya KVKK'nın
+                belirlediği diğer yöntemlerle yapabilirsiniz.
+              </p>
+            </section>
+          </div>
         </div>
       </main>
       <Footer />
